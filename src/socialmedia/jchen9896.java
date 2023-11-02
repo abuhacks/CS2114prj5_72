031bb15b42bd7ead462a76d1fa61247f42948345
narenDaw <narend@vt.edu>
2023-11-02 15:03:37 -0400

    Update jchen9896.java

---
+++ src/socialmedia/jchen9896.java
@@ -0,0 +1,14 @@
+package socialmedia;
+public class jchen9896
+{
+    //~ Fields ................................................................
+
+    //~ Constructors ..........................................................
+
+    //~Public  Methods ........................................................
+    public static void main(String[] args) {
+
+        System.out.println("Glad to be on your team");
+
+    }
+}
