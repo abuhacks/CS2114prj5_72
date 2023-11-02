9813d8be2f14106ce52edc81ce5a3b339606afe1
Jchen9896 <jchen9896@vt.edu>
2023-11-02 15:03:55 -0400

    Update abielm.java

---
+++ src/socialmedia/abielm.java
@@ -0,0 +1,15 @@
+package socialmedia;
+
+public class abielm
+{
+    // ----------------------------------------------------------
+    /**
+     * Place a description of your method here.
+     */
+    public static void main(String[] args)
+    {
+
+        System.out.println("Glad to be on your team");
+
+    }
+}
