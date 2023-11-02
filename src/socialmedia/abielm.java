fd545316787f9e7683db490bb34f79ed421ed098
abuhacks <abu.school64@gmail.com>
2023-11-02 15:13:18 -0400

    Update

--- src/socialmedia/abielm.java
+++ src/socialmedia/abielm.java
@@ -7,7 +7,7 @@ public class abielm {
      */
     public static void main(String[] args) {
 
-        System.out.println("Glad to be on your team, from Colby abiel");
+        System.out.println("Glad to be on your team, from Colby narend abiel");
 
     }
 }
