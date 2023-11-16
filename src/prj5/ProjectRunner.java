088f89d521cff4dcae504ba84b236c2c3c9cc974
abuhacks <abu.school64@gmail.com>
2023-11-16 14:33:10 -0500

    A quick lil ProjRunner Update
    Project Runner complete

--- src/prj5/ProjectRunner.java
+++ src/prj5/ProjectRunner.java
@@ -3,19 +3,26 @@
  */
 package prj5;
 
+import java.io.IOException;
 
 /**
- * 
+ * This is the ProjectRunner
+ * Class that takes in
+ * the InputFileReader
+ * and reads the Sample
+ * Input and spits out an output.
  */
-public class ProjectRunner 
-{
-    InputFileReader filer;
-    
+public class ProjectRunner {
 
-    if (args.length > 0) {
-    filer = new InputFileReader(args[0]); 
+    public static void main(String[] args) throws IOException {
+        InputFileReader filer;
+
+        if (args.length > 0) {
+            filer = new InputFileReader(args[0]);
+        }
+        else {
+            filer = new InputFileReader("SampleInput1_2023.csv");
+        }
     }
-    else {
-    filer = new InputFileReader("SampleInput1_2023.csv");
-    }
+
 }
