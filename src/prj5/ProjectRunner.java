6112a4d0061ccdf727325baa18d8de799594befb
narenDaw <narend@vt.edu>
2023-11-18 20:04:59 -0500

    Update ProjectRunner.java

--- src/prj5/ProjectRunner.java
+++ src/prj5/ProjectRunner.java
@@ -9,40 +9,52 @@ import java.io.IOException;
  * This is the ProjectRunner Class that takes in the InputFileReader and reads
  * the Sample Input and spits out an output.
  */
-public class ProjectRunner {
+public class ProjectRunner
+{
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)
+        throws IOException
+    {
         InputFileReader filer;
-        
 
-        if (args.length > 0) {
+        if (args.length > 0)
+        {
             filer = new InputFileReader(args[0]);
         }
-        else {
+        else
+        {
             filer = new InputFileReader("SampleInput1_2023.csv");
         }
         boolean showConsole = true;
         boolean showGUI = false;
-        if (showConsole) {
+        if (showConsole)
+        {
             SortingCalculator newTemp = new SortingCalculator(filer.getList());
             DoublyLinkedList<User> newList = newTemp.sortByName();
             DoublyLinkedList<User> newList2 = newTemp.sortByReachRate();
-            for(int i = 0; i < newList.getLength(); i++) {
+            for (int i = 0; i < newList.getLength(); i++)
+            {
                 System.out.println(newList.getEntry(i).getChannelName());
-                System.out.println("traditional: " + newTemp.getTraditionalRate(newList.getEntry(i)));
+                System.out.println(
+                    "traditional: "
+                        + newTemp.getTraditionalRate(newList.getEntry(i)));
                 System.out.println("==========");
-                if(i == newList.getLength() - 1) {
+                if (i == newList.getLength() - 1)
+                {
                     System.out.println("**********");
                     System.out.println("**********");
                 }
             }
-            for(int j = 0; j < newList2.getLength(); j++) {
+            for (int j = 0; j < newList2.getLength(); j++)
+            {
                 System.out.println(newList2.getEntry(j).getChannelName());
-                System.out.println("reach: " + newTemp.getReachRate(newList2.getEntry(j)));
+                System.out.println(
+                    "reach: " + newTemp.getReachRate(newList2.getEntry(j)));
                 System.out.println("==========");
             }
         }
-        if (showGUI) {
+        if (showGUI)
+        {
             /*
              * TODO: For final Submission - Should have the GUI display for the
              * input data
